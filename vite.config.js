060bda6850e2581189859966b2119c3b005c6b4b
aleynakircali <aleynakirc@gmail.com>
2026-09-12 18:39:01 +0300

    update portfolio site

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        computation: resolve(process.cwd(), 'computation.html'),
+      },
+    },
+  },
 });
